7986997c083b372d4caec18226364d6816d1af7a
Hailey Sampson <hsampson2024@gmail.com>
2026-03-02 17:37:33 -0400

    added hello

--- app/main.py
+++ app/main.py
@@ -35,4 +35,5 @@ async def unauthorized_redirect_handler(request: Request, exc: Exception):
 
 
 if __name__ == "__main__":
+    print("hello world")
     uvicorn.run("app.main:app", host=get_settings().app_host, port=get_settings().app_port, reload=get_settings().env.lower()!="production")
